3c0eb6e21496180a7248d664e3d80bc29ce8cfa7
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2018-01-11 13:43:31 +0100

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from '@/components/App';
+import HelloWorld from '@/components/HelloWorld';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: (to) => {
+        /* eslint no-console: ["error", { allow: ["log"] }] */
+        if (to.params.lang !== 'en' || to.params.lang !== 'de') {
+          const language = window.navigator.userLanguage || window.navigator.language;
+          return `/${language.split('-')[0]}`;
+        }
+        return to.fullPath;
+      },
+    },
+    {
+      path: '/:lang',
+      components: {
+        default: App,
+      },
+      // Children to the root path '/'
+      children: [
+        {
+          path: 'start',
+          name: 'start',
+          components: {
+            Content: HelloWorld,
+          },
+        },
+        {
+          path: '*',
+          redirect: 'start',
+        },
+      ],
+    },
+  ],
+});
